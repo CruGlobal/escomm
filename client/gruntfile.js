6a52b00de358f41d82ad99da21716d25b15c01ca
Asaph Yuan <asaph.yuan@gmail.com>
2016-01-05 13:53:02 -0800

    added angular-gettext, only working for homepage

---
+++ client/gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        nggettext_extract: {
+            pot: {
+                files: {
+                    'po/template.pot': ['src/app/views/*.html']
+                }
+            },
+        },
+        nggettext_compile: {
+          all: {
+            files: {
+              'src/js/translations.js': ['po/*.po']
+            }
+          },
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-angular-gettext');
+    grunt.registerTask('default', ['nggettext_extract', 'nggettext_compile']);
+};
---
+++ client/src/js/translations.js
@@ -0,0 +1,5 @@
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('hu', {"Help New Believers Grow":"Segíts Új hívők Grow","Invite them to a personal email series from you. Every few days, they will learn from you important truths about their new relationship with God.":"Kérd meg őket , hogy egy személyes e-mail -sorozat tőled . Pár nap , akkor tanulni tőled fontos igazságokat az új kapcsolat Istennel ."});
+/* jshint +W100 */
+}]);
