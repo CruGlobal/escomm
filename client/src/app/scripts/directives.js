556717359d2ae1c7f83e448b409508e33cf1af3e
Asaph Yuan <asaph.yuan@gmail.com>
2016-03-17 13:01:46 -0400

    invite-feedback

--- client/src/app/scripts/directives.js
+++ client/src/app/scripts/directives.js
@@ -1,7 +1,30 @@
 angular.module('escomm')
-.controller('MainCtrl', function ($scope, $http, $location) {
+
+.service('inviteSuccess', function inviteSuccess() {
+  var inviteSuccess = this;
+
+  inviteSuccess.message = false;
+  inviteSuccess.fname = "";
+})
+
+.controller('MainCtrl', function ($scope, $http, $location, inviteSuccess) {
+
   $scope.FormData = {};
+  $scope.isDisabled = false;
+  $scope.showSendingBtn = false;
+  $scope.showSendBtn = true;
+  $scope.showErrorMsg = false;
+  var first = this;
+  first.inviteSuccess = inviteSuccess;
+  $scope.sendInviteSuccess = first.inviteSuccess.message;
+  $scope.fname = first.inviteSuccess.fname;
+
   $scope.sendInvite = function() {
+    //disable/show button
+    $scope.isDisabled = true;
+    $scope.showSendBtn = false;
+    $scope.showSendingBtn = true;
+
     $scope.FormData.first_name = $scope.firstName;
     $scope.FormData.last_name = $scope.lastName;
     $scope.FormData.email_address = $scope.emailAddress;
@@ -12,8 +35,16 @@ angular.module('escomm')
 
     $http.post("https://crucore.com/api.php?a=invite", angular.toJson($scope.FormData)).success(function(data, status) {
       console.log("Success")
+      
+      first.inviteSuccess.message = true;
+      first.inviteSuccess.fname = $scope.firstName;
+      console.log (first.inviteSuccess.fname);
       $location.path( '/contacts_activity' );
     })
+    .error(function(data, status){
+      console.log("Error")
+      $scope.showSendingBtn = false;
+      $scope.showErrorMsg = true;
+    });
   }
 })
-
